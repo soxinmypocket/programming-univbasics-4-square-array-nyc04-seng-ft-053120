efd96788d6655b7c2edeff6640f728df13ffd14d
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 13:52:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,9 @@ def square_array(array)
   numbers = [1, 2, 3]
   for value in numbers do
     value * value
-
+end
   new_numbers = [9, 10, 16, 25]
   for number in new_numbers do
     value * value
   end
-end
 end
