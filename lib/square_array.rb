c146bd5820ed805a2affb059bb4b6a6cd91e01ce
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 14:20:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(numbers)
   numbers = [1, 2, 3]
-    while numbers < numbers.length do
+    while numbers > numbers.length do
       numbers ** 2
     square_array  << numbers
     end
