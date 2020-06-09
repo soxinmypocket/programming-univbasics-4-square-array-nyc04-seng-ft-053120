45dafd2e86a7359613d6f33c84ba8e9fb72b9420
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 13:34:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,6 @@
-def square_array(numbers)
-  numbers = [1,2,3]
-  square_array(numbers)
-
-  new_numbers = [9, 10, 16, 25]
-
-  square_array(new_numbers)
+def square_array(array)
+  number = [1,2,3]
+  number.collect do |i|
+    square = i*i
+  end
 end
